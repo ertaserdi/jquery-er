5cee41b00b75bddf5c38b1dce1e34f9601603023
erdi <ertaserdi@gmail.com>
2017-02-22 13:10:59 +0300

    Beta

--- src/jquery-er.js
+++ src/jquery-er.js
@@ -16,7 +16,20 @@ var er = {
         return r;
     },
     count: function (a) {
-        return this.length;
+        if (typeof a == 'Array') {
+            return this.length;
+        }else if (typeof a == 'Object') {
+            return Object.keys(field).length;
+        }
+    },
+    isset : function (v) {
+        return this.count(v) > 0
+    },
+    ucfirst : function (str) {
+        str = str.toLowerCase().replace(/\b[a-z]/g, function(letter) {
+            return letter.toUpperCase();
+        });
     }
+
 };
 
